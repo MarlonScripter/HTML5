e10cd2e1756d112fbbb992d065cb0f5ae9053285
Marlon silva araujo <marlonaraujo01303@gmail.com>
2024-04-15 19:37:36 -0300

    formato

--- Code/scripts.js
+++ Code/scripts.js
@@ -1,24 +1,25 @@
-let chave = 'cebcd482eda57fa9a6714c1c2ba91885'
+let chave = "cebcd482eda57fa9a6714c1c2ba91885";
 
 function colocarNaTela(dados) {
-  console.log(dados)
+  console.log(dados);
 
-  document.querySelector('.cidade').innerHTML = 'Tempo em ' + dados.name
-  document.querySelector('.temp').innerHTML = Math.floor(dados.main.temp) + '°C'
-  document.querySelector('.icone').src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+  document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name;
+  document.querySelector(".temp").innerHTML =
+    Math.floor(dados.main.temp) + "°C";
+  document.querySelector(".icone").src =
+    "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png";
 }
 
-async function buscarcidade(cidade){
+async function buscarcidade(cidade) {
   let dados = await fetch(
-    'https://api.openweathermap.org/data/2.5/weather?q=' +
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
       cidade +
-      '&appid=cebcd482eda57fa9a6714c1c2ba91885&units=metric',
-  ).then((resposta) => resposta.json())
-
+      "&appid=cebcd482eda57fa9a6714c1c2ba91885&units=metric"
+  ).then((resposta) => resposta.json());
 }
 
 function clicarbotao() {
-  let cidade = document.querySelector(".inputCidade").value
+  let cidade = document.querySelector(".inputCidade").value;
 
-  buscarcidade(cidade)
+  buscarcidade(cidade);
 }
